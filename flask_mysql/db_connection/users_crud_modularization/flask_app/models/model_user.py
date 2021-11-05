30d3a2f51de8111fdb7408145981b66b57c7e602
Teressa Sung <teressas.hawaii@gmail.com>
2021-11-04 23:19:42 -0700

    initial commit

---
+++ flask_mysql/db_connection/users_crud_modularization/flask_app/models/model_user.py
@@ -0,0 +1,52 @@
+# import the function that will return an instance of a connection
+from flask_app.config.mysqlconnection import connectToMySQL
+
+DATABASE = 'users'
+# model the class after the user table from our database
+class User:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    # Now we use class methods to query our database
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        # connectToMySQL is class, ('users') is an instance of a class
+        results = connectToMySQL(DATABASE).query_db(query)
+        # Create an empty list to append our instances of users
+        users = []
+        # Iterate over the db results and create instances of users with cls.
+        for user in results:
+            users.append( cls(user) )
+        return users
+
+        # class method to save our user to the DATABASE
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(first_name)s , %(last_name)s , %(email)s , NOW() , NOW() );"
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('users').query_db( query, data )
+    
+    @classmethod
+    def get_one(cls, data:dict):
+        query = "SELECT * FROM users WHERE id=%(id)s;"
+        # LIST OF DICTIONARIES
+        results = connectToMySQL(DATABASE).query_db(query, data)
+        if not results:
+            return False
+        return cls(results[0])
+
+    @classmethod
+    def update_one(cls, data):
+        query = "UPDATE users SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s WHERE id = %(id)s;"
+        return connectToMySQL(DATABASE).query_db(query,data)
+
+    @classmethod
+    def delete_one(cls,data):
+        query  = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL(DATABASE).query_db(query,data)
